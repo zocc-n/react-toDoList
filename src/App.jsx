1bcd5f4a0edc6d9254507ce8d2606acf7269c2b3
zocc-n <znanic@yahoo.com>
2023-06-18 02:20:05 +0200

    working app; one component only

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,78 @@ import "./styles.css"
 import { useState } from 'react'
 
 const App = () => {
+
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setTodos(currentTodos => {
+      console.log(todos)
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    })
+    setNewItem("")
+  }
+
+  const toggleTodo = (id, completed) => {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  } 
+
+  const deleteTodo = id => {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return(
     <>
-    
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+            value={newItem} 
+            onChange={e => setNewItem(e.target.value)} 
+            type="text" 
+            id="item" 
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      
+      <h1 className="header">Todo List</h1>
+      
+      <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return(
+          <li key={todo.id}>
+            <label>
+              <input 
+                type="checkbox" 
+                checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.checked)} 
+              />
+              {todo.title}
+            </label>
+            <button 
+              className="btn btn-danger" 
+              onClick={() => deleteTodo(todo.id)}>
+                Delete
+            </button>
+          </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
